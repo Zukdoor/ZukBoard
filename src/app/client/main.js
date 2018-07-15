949cf72b29dbdce05ae860cbb3ae24c3a3d4ca28
qianyuchang <qianyuchang@gmail.com>
2018-07-15 15:11:01 +0800

    deploy build

--- src/app/client/main.js
+++ src/app/client/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import './assets/element-variables.scss'
 import VueResource from 'vue-resource'
-import 'ionicons/dist/css/ionicons.min.css'
+import '../client/assets/ionicons.css'
 
 import App from './App'
 import router from './router'
