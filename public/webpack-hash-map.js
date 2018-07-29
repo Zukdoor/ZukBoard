10b341de99f9f99f447831e6e88ff149d95d40d6
qianyuchang <qianyuchang@gmail.com>
2018-07-29 22:42:59 +0800

    deploy build

--- public/webpack-hash-map.js
+++ public/webpack-hash-map.js
@@ -1,1 +1,1 @@
-module.exports = {"css":{"app":"dist/css/app.0ab97afc1c0025d69637.css"},"js":{"manifest":"dist/js/manifest.c8b7d4a38e1cdb15655b.js","vendor":"dist/js/vendor.71809370bb660d6b571c.js","app":"dist/js/app.99f4c8a703f033545902.js"}}
+module.exports = {"css":{"app":"dist/css/app.fd93e6849a4893069833.css"},"js":{"manifest":"dist/js/manifest.c8b7d4a38e1cdb15655b.js","vendor":"dist/js/vendor.71809370bb660d6b571c.js","app":"dist/js/app.1db754822ffda18c13e5.js"}}
