c6cf39a6ca4824d04a999a41b5bc6c9c0783afb7
mac <mac@macdeMacBook-Pro.local>
2018-10-16 22:21:37 +0800

    当按住shiftkey选中多个元素时，然后点击空白区域，导致反选

--- src/app/client/pages/canvas/draw/plugins/fabricOverriding.js
+++ src/app/client/pages/canvas/draw/plugins/fabricOverriding.js
@@ -31,6 +31,26 @@ fabric.Object.prototype._setCornerCoords = function () {
     }
   }
 }
+fabric.Canvas.prototype._shouldClearSelection = function (e, target) {
+  let activeObjects = this.getActiveObjects()
+
+  let activeObject = this._activeObject
+
+  return (
+    (!target && !e.shiftKey) ||
+    (target &&
+      activeObject &&
+      activeObjects.length > 1 &&
+      activeObjects.indexOf(target) === -1 &&
+      activeObject !== target &&
+      !this._isSelectionKeyPressed(e)) ||
+    (target && !target.evented) ||
+    (target &&
+      !target.selectable &&
+      activeObject &&
+      activeObject !== target)
+  )
+}
 const container = document.querySelector('body')
 fabric.util.addListener(container, 'keydown', function (e) {
   if (e.code === 'Space') {
