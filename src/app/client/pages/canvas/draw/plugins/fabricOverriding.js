c7c6e0d9626b01e4f83ded12ae1d85f0051afa95
mac <mac@macdeMacBook-Pro.local>
2018-10-16 19:04:00 +0800

    backspace to move canvas

--- src/app/client/pages/canvas/draw/plugins/fabricOverriding.js
+++ src/app/client/pages/canvas/draw/plugins/fabricOverriding.js
@@ -3,7 +3,7 @@ import { fabric } from 'fabric'
 fabric.Object.prototype._setCornerCoords = function () {
   const coords = this.oCoords
   const newTheta = fabric.util.degreesToRadians(45 - this.angle)
-  const cornerHypotenuse = this.cornerSize * 2
+  const cornerHypotenuse = this.cornerSize * 1.5
   const cosHalfOffset = cornerHypotenuse * fabric.util.cos(newTheta)
   const sinHalfOffset = cornerHypotenuse * fabric.util.sin(newTheta)
   let x, y
@@ -31,3 +31,14 @@ fabric.Object.prototype._setCornerCoords = function () {
     }
   }
 }
+const container = document.querySelector('body')
+fabric.util.addListener(container, 'keydown', function (e) {
+  if (e.code === 'Space') {
+    window.spaceDown = true
+  }
+})
+fabric.util.addListener(container, 'keyup', function (e) {
+  if (e.code === 'Space') {
+    window.spaceDown = false
+  }
+})
