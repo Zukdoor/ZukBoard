fd9a109d1c19e4ff662d725d995d538dc1b2c925
Gabriel Wu <qqbbnease1004@126.com>
2018-12-07 11:04:52 +0800

    fix: remove vuex-persist and build is OK

--- src/app/client/store/index.js
+++ src/app/client/store/index.js
@@ -1,19 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import VuexPersistence from 'vuex-persist'
 import user from './modules/user'
 Vue.use(Vuex)
-
-const vuexLocal = new VuexPersistence({
-  storage: window.localStorage,
-  modules: [
-    'user'
-  ]
-})
 
 export default new Vuex.Store({
   modules: {
     user
-  },
-  plugins: [vuexLocal.plugin]
+  }
 })
